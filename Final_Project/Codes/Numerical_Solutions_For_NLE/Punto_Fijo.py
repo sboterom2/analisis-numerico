caae2c9bc2ce503202b4116b3b2bb65033eb6409
sboterom2 <85262481+sboterom2@users.noreply.github.com>
2022-11-10 14:26:08 -0500

    Add files via upload

---
+++ Final_Project/Codes/Numerical_Solutions_For_NLE/Punto_Fijo.py
@@ -0,0 +1,19 @@
+#Metodo de Punto Fijo
+import numpy as np 
+def g(p): 
+  return 0.5*pow(10.0-p*p*p, 0.5) 
+
+def punto_fijo(p0,NO,TOL):
+    i=0
+    while i <= NO:
+       p=g(p0)
+       if np.abs(p0-p)<TOL:
+          break
+       print(i, "numero de iteracion", " ", p)
+       i+=1
+    p0=p
+ 
+    if(i==NO+1):
+      print("El metodo fallo")
+    else:
+      print("El metodo convergio")
