88ac13e5f903b7c0398c863bf50687373a60fe26
sboterom2 <85262481+sboterom2@users.noreply.github.com>
2022-11-21 10:36:25 -0500

    Update multiple_roots.py

--- Final_Project/Codes/Numerical_Solutions_For_NLE/multiple_roots.py
+++ Final_Project/Codes/Numerical_Solutions_For_NLE/multiple_roots.py
@@ -6,19 +6,19 @@ def function(eq):
     global x
     return sp.sympify(eq)
 
-def Raices_Multiples(eq,x0,tol,it):
+def multiple_roots(eq,x0,tol,it):
     global x
     eq=function(eq)
-    derivada1 = sp.diff(eq,x)
-    derivada2 = sp.diff(derivada1,x)
+    d1 = sp.diff(eq,x)
+    d2 = sp.diff(d1,x)
 
     c=0
     e=tol+1
     
 
-    while(c < it and e > tol and ((pow(derivada1.evalf(subs={x:x0}), 2)-(eq.evalf(subs={x:x0})*derivada2.evalf(subs={x:x0})))!=0)):
+    while(c < it and e > tol and ((pow(de1.evalf(subs={x:x0}), 2)-(eq.evalf(subs={x:x0})*d2.evalf(subs={x:x0})))!=0)):
         c=c+1
-        t=x0-((eq.evalf(subs={x:x0})*derivada1.evalf(subs={x:x0}))/(pow(derivada1.evalf(subs={x:x0}),2)-(eq.evalf(subs={x:x0})*derivada2.evalf(subs={x:x0}))))
+        t=x0-((eq.evalf(subs={x:x0})*d1.evalf(subs={x:x0}))/(pow(d1.evalf(subs={x:x0}),2)-(eq.evalf(subs={x:x0})*d2.evalf(subs={x:x0}))))
         e=abs(x0-t)
         x0=t
     print(x0)
