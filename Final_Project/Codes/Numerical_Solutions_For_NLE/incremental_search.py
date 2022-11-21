a4fb509c3329576d513d6c09c79bf9c63adbb6e0
sboterom2 <85262481+sboterom2@users.noreply.github.com>
2022-11-21 18:30:48 -0500

    Update incremental_search.py

--- Final_Project/Codes/Numerical_Solutions_For_NLE/incremental_search.py
+++ Final_Project/Codes/Numerical_Solutions_For_NLE/incremental_search.py
@@ -1,6 +1,5 @@
 import sympy as sp
 
-
 x=sp.symbols('x')
 
 def funcion(ecua):
@@ -9,24 +8,30 @@ def funcion(ecua):
 
 def Incremental_Search(eq, a, b, deltaX):
     global x
-    eq=funcion(ecuacion)
+    eq=funcion(eq)
+    it=0
 
     while(a<b):
         xi = a + deltaX
-        fun_a = eq.evalf(subs={x:a})
-        
+        fun_a = eq.evalf(subs={x:a})    
         fun_xi = eq.evalf(subs={x:xi})
         if (fun_a *fun_xi )<0:    
-            
-            print(a)
-            print(xi)
-            
+          it=it+1         
+          break
         else:
+            it=it+1
             a=xi
-  
+    print('The solution is in the interval ['+str(a)+', '+ str(xi)+']')
+    
+    return a,xi,it
+          
+func = input("Put the function: ")
+a = float(input("put a: "))
+b = float(input("Put b: "))
+dx = float(input("pt dx: "))
 
-  
-res = Incremental_Search('sin(x)',-7,7,0.3)
+Incremental_Search(func,a,b,dx)
+
 
 
 
