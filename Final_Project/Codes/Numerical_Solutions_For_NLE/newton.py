40a9e8286fddf0e54e29a6d713582c99e32911de
sboterom2 <85262481+sboterom2@users.noreply.github.com>
2022-11-21 19:10:15 -0500

    Update newton.py

--- Final_Project/Codes/Numerical_Solutions_For_NLE/newton.py
+++ Final_Project/Codes/Numerical_Solutions_For_NLE/newton.py
@@ -1,6 +1,5 @@
 import sympy as sp
 
-
 x=sp.symbols('x')
 
 def function(eq):
@@ -23,6 +22,13 @@ def Newton(eq,x_0,es,maxIte):
         inter+=1
         
         if inter>=maxIte:
-            print("The method does not converge")
             break
+        
+    print('The solution is '+str(x_r)+', with an eror of '+str(ea)+', in '+ str(inter)+ ' iterations')
+    
+fun=input("Put the function: ")
+x_0=float(input("Put X0: "))
+tol=float(input("Put tolerance: "))
+maxIte=int(input("Put max iterations: "))
+Newton(fun,x_0,tol,maxIte)
     
