f47562a48f3d678277c60beb042add248ba14de9
sboterom2 <85262481+sboterom2@users.noreply.github.com>
2022-11-21 19:02:50 -0500

    Update multiple_roots.py

--- Final_Project/Codes/Numerical_Solutions_For_NLE/multiple_roots.py
+++ Final_Project/Codes/Numerical_Solutions_For_NLE/multiple_roots.py
@@ -16,12 +16,19 @@ def multiple_roots(eq,x0,tol,it):
     e=tol+1
     
 
-    while(c < it and e > tol and ((pow(de1.evalf(subs={x:x0}), 2)-(eq.evalf(subs={x:x0})*d2.evalf(subs={x:x0})))!=0)):
+    while(c < it and e > tol and ((pow(d1.evalf(subs={x:x0}), 2)-(eq.evalf(subs={x:x0})*d2.evalf(subs={x:x0})))!=0)):
         c=c+1
         t=x0-((eq.evalf(subs={x:x0})*d1.evalf(subs={x:x0}))/(pow(d1.evalf(subs={x:x0}),2)-(eq.evalf(subs={x:x0})*d2.evalf(subs={x:x0}))))
         e=abs(x0-t)
         x0=t
-    print(x0)
-    print(e)
-   
+    print('The solution is'+str(x0)+', with an error of '+str(e))
+    
+    
 
+
+fun = input("Ingresa la funcion: ")
+x0 = float(input("Ingresa X0: "))
+tol = float(input("Ingresa la tolerancia: "))
+ite = float(input("Ingresa las iteraciones: "))
+
+multiple_roots(fun,x0,tol,ite)
